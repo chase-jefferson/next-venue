301f80b4d8846cfb08306fffad789db2f172f766
Warren <wrenwestart@gmail.com>
2025-02-24 21:35:42 -0500

    Adding navbar, main page, and profile page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,60 +1,32 @@
-import { useState, useEffect, useLayoutEffect } from "react";
-import { retrieveUsers } from "../api/userAPI";
-import type { UserData } from "../interfaces/UserData";
-import ErrorPage from "./ErrorPage";
-import UserList from '../components/Users';
-import auth from '../utils/auth';
+import React from "react";
 
-const Home = () => {
-
-    const [users, setUsers] = useState<UserData[]>([]);
-    const [error, setError] = useState(false);
-    const [loginCheck, setLoginCheck] = useState(false);
-
-    useEffect(() => {
-        if (loginCheck) {
-            fetchUsers();
-        }
-    }, [loginCheck]);
-
-    useLayoutEffect(() => {
-        checkLogin();
-    }, []);
-
-    const checkLogin = () => {
-        if (auth.loggedIn()) {
-            setLoginCheck(true);
-        }
-    };
-
-    const fetchUsers = async () => {
-        try {
-            const data = await retrieveUsers();
-            setUsers(data)
-        } catch (err) {
-            console.error('Failed to retrieve tickets:', err);
-            setError(true);
-        }
-    }
-
-    if (error) {
-        return <ErrorPage />;
-    }
-
-    return (
-        <>
-            {
-                !loginCheck ? (
-                    <div className='login-notice'>
-                        <h1>
-                            Login to view all your friends!
-                        </h1>
-                    </div>
-                ) : (
-                    <UserList users={users} />
-                )}
-        </>
-    );
+const Home: React.FC = () => {
+  return (
+    <div className="flex flex-auto items-center justify-center min-h-screen bg-linear-to-br from-orange-300 to-yellow-500">
+      <div className="flex max-w-3xl max-h p-5 rounded-2xl">
+        <img
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.wallpapersafari.com%2F98%2F90%2FmjHyV5.jpg&f=1&nofb=1&ipt=b349f151cd35da8a3a239a460785937dc558f6551fcc4619543f88eec11499ba&ipo=images"
+          alt="Concert"
+          className="w-screen rounded-xl shadow-lg"
+        />
+      </div>
+      <div className="flex max-w-2xl max-h text-center bg-stone-500/40 p-8 rounded-2xl shadow-lg mt-6">
+        <div>
+          <h1 className="text-5xl font-bold text-amber-900/70 mb-4 font-sans">
+            Welcome to Next Venue
+          </h1>
+          <p className="text-lg text-black mb-6 font-sans">
+            Discover and plan your next unforgettable event with ease. Check for
+            weather updates and create RSVPs for you and your guests. Log in or
+            register today to plan your next outing!
+          </p>
+          <button className="bg-amber-700 text-white px-6 py-3 rounded-xl text-lg font-semibold shadow-md hover:bg-amber-900 transition">
+            Create Event
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/auth': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // https://vitejs.dev/config/
+  plugins: [tailwindcss()],
 });
