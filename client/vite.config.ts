7d16a622c13f8fd9f7a0a80fab5f99483df73d9c
Chase Jefferson <chasejefferson@Chases-MacBook-Air.local>
2025-02-24 20:30:06 -0500

    updated client-side changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
-import tailwindcss from 'vite-plugin-tailwindcss'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
   server: {
     port: 3000,
     open: true,
